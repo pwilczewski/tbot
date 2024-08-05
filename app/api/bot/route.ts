d0bd35f2c5b68ba70685096ff6422a9c8b5901dd
pwilczewski <wilczewski@gmail.com>
2024-08-05 18:13:15 -0500

    added openai

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -3,13 +3,33 @@ import { Bot, webhookCallback } from "grammy";
 
 const token = process.env.TELEGRAM_BOT_TOKEN as string
 
+// add an openai call here
+
+import OpenAI from 'openai';
+
+
+
 export const POST = async (req: NextRequest) => {
 
   const bot = new Bot(token)
+  const openai = new OpenAI();
 
   bot.on("message", async (ctx) => {
-    await ctx.reply("...");
+    // await ctx.reply("...");
+    const message = ctx.message.text as string;
+    const chatId = ctx.chatId;
+
+    const resp = await openai.completions.create({
+        model: 'text-davinci-002',
+        prompt: message,
+        max_tokens: 50,
+        temperature: 0,
+      });
+
+    await bot.api.sendMessage(chatId, resp.choices[0].text);
   });
+
+
 
   const handleUpdate = webhookCallback(bot, "std/http");
 
@@ -17,5 +37,5 @@ export const POST = async (req: NextRequest) => {
 };
 
 
-// probably the missing piece - how does telegram know where to go?
 // curl https://api.telegram.org/bot<telegram_bot_token>/setWebhook?url=https://<your-deployment.vercel>.app/api/bot
+// curl https://api.telegram.org/bot6893250826:AAEdaWjzGzFN8-vrnrTLhJ7DybU--FVGzzs/setWebhook?url=https://tbot-tau.vercel.app/api/bot
