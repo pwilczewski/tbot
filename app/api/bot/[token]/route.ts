1b5acb62504661b8ec2894d2d936727f69ffac6d
pwilczewski <wilczewski@gmail.com>
2024-08-20 12:27:47 -0500

    adding langchain rag

--- app/api/bot/[token]/route.ts
+++ app/api/bot/[token]/route.ts
@@ -6,8 +6,11 @@ import { userStatus, users } from "@prisma/client";
 import { NextRequest } from "next/server";
 import { createClient } from '@supabase/supabase-js'
 import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
-import { OpenAIEmbeddings } from "@langchain/openai";
+import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
 import type { Document } from "@langchain/core/documents";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 
 const openai = new OpenAI();
 
@@ -36,9 +39,7 @@ async function addEmbeddings (questionId: bigint) {
 
   const embeddings = new OpenAIEmbeddings({model: "text-embedding-3-small",});
   const vectorStore = new SupabaseVectorStore(embeddings, {
-    client: client,
-    tableName: "documents",
-    queryName: "match_documents",
+    client: client, tableName: "documents", queryName: "match_documents",
   });
 
   const doc1: Document = {pageContent: convo, metadata: {source: "testing"}}
@@ -188,12 +189,43 @@ export const POST = async (req: NextRequest) => {
           await prismadb.userStatus.update({where: {id: cuserStatus.id}, data: {questionId: question.id, question: question.question}})
         }
       } else if (cuserStatus.status==="chat") {
-        // rag it! do i need / want langchain for this part
+
+        const client = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, 
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string)
+        const embeddings = new OpenAIEmbeddings();
+        const vectorStore = new SupabaseVectorStore(embeddings, {client, tableName: "documents",});
+        const retriever = vectorStore.asRetriever();
+
+        //           So far the user {name} has discussed with you (system) {chatHistory}
+        const prompt = ChatPromptTemplate.fromTemplate(
+          `You are answering questions on behalf of Paul.
+          Now answer the user's questions in the first person using the context available. 
+          If the answer is not available in the context don't make up an answer just reply: I don't know. \n
+          Context\n{context}\n Question:\n{question}`
+        );
+
+        const llm = new ChatOpenAI({ model: "gpt-4o-mini", temperature: 0 });
+        const retrievedDocs = await retriever.invoke("what is task decomposition");
+        
+        const ragChain = await createStuffDocumentsChain({
+          llm, prompt,
+          outputParser: new StringOutputParser(),
+        });
+
+        const resp = await ragChain.invoke({
+          question: "What is task decomposition?",
+          context: retrievedDocs,
+        });
+
+        // const retriever = vectorStore.asRetriever({filter: (rpc: SupabaseFilter) => rpc.filter("metadata->>repHandle", "eq", rep.repHandle)});
+
+        /*
         const repQ = await openai.chat.completions.create({
           messages: [{ role: "system", content: "Give a very short reply to the user's question."},
               {role: "user", content: message }],
           model: 'gpt-4o-mini', })
         const resp = repQ.choices[0].message.content as string
+        */
         await bot.api.sendMessage(chatId, resp)
       }
 
