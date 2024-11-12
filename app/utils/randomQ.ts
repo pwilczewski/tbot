ec37d515acc9608e168ae6afcaf53fbd3816ee55
pwilczewski <wilczewski@gmail.com>
2024-11-11 20:04:07 -0600

    more nulls removed

--- app/utils/randomQ.ts
+++ app/utils/randomQ.ts
@@ -1,29 +1,20 @@
 import prismadb from "@/lib/prismadb";
 import { userStatus } from "@prisma/client";
 
-export async function randomQ(answeredQs: {questionId: number | null}[], cuserStatus: userStatus | null) {
+export async function randomQ(answeredQs: {questionId: number}[], cuserStatus: userStatus) {
 
-    let excludeQs: number[] = [];
-    if (answeredQs!==null) {
-      excludeQs = answeredQs.map(item => item.questionId).filter((id): id is number => id !== null);
-    }
-  
-    let userQLevel: number = 0;
-    if (cuserStatus!==null) {
-      userQLevel = cuserStatus.questionLevel as number
-    }
-  
+    let excludeQs: number[] = answeredQs.map(item => 
+      item.questionId).filter((id): id is number => id !== null);
+    let userQLevel: number = cuserStatus.questionLevel as number;
     let count = await prismadb.basedQuestions.count({where: {questionLevel: userQLevel, id: {notIn: excludeQs}}});
   
     // if all questions have been answered or skipped, advance level
     if (count===0) {
-      if (cuserStatus!==null) {
-        if (cuserStatus.questionLevel as number < 5) {
-          await prismadb.userStatus.update({ where: {id: cuserStatus.id}, data: {questionLevel: cuserStatus.questionLevel as number + 1} })
-        }
-        userQLevel = cuserStatus.questionLevel as number
-        count = await prismadb.basedQuestions.count({where: {questionLevel: userQLevel, id: {notIn: excludeQs}}})
+      if (cuserStatus.questionLevel as number < 5) {
+        await prismadb.userStatus.update({ where: {id: cuserStatus.id}, data: {questionLevel: cuserStatus.questionLevel as number + 1} })
       }
+      userQLevel = cuserStatus.questionLevel as number
+      count = await prismadb.basedQuestions.count({where: {questionLevel: userQLevel, id: {notIn: excludeQs}}})
     }
   
     const randomOffset = Math.floor(Math.random() * count);
